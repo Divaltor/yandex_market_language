77bf9564463ecc1fa34d6014f75eb78b6f9998ca
Divaltor <kuklavudu78@gmail.com>
2020-07-29 14:31:51 +0300

    Fixed price (may be)

--- yandex_market_language/models/offers.py
+++ yandex_market_language/models/offers.py
@@ -115,7 +115,7 @@ class AbstractOffer(
         self.bid = bid
         self.cbid = cbid
         self.url = url
-        self.price = price
+        self.price = price or Price(0)
         self.old_price = old_price
         self.enable_auto_discounts = enable_auto_discounts
         self.currency = currency
@@ -287,7 +287,7 @@ class AbstractOffer(
             offer_id=self.offer_id,
             bid=self.bid,
             url=self.url,
-            price=self.price.to_dict() if self.price else Price(0).to_dict(),
+            price=self.price.to_dict(),
             old_price=self.old_price,
             enable_auto_discounts=self.enable_auto_discounts,
             currency=self.currency,
